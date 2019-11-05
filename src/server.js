b701c40ce240ddc2f723faa2fe62fc1551213b76
herick1 <herick200@gmail.com>
2019-11-05 09:32:33 -0400

    actualizando hacia heroku 6

--- src/server.js
+++ src/server.js
@@ -815,8 +815,4 @@ app.delete("/*", (req, res) => {
   res.status(404).send();
 });
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+http.createServer(onRequest).listen(process.env.PORT || 6000)
